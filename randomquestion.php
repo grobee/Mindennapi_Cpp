83d9b965ee5c46eedf60f9431a474acc24263f29
Birks <tommyb1@windowslive.com>
2014-11-26 21:56:26 +0100

    JSON generator and submitquestion interface added

---
+++ randomquestion.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tamás
+ * Date: 11/26/2014
+ * Time: 6:32 PM
+ */
+
+$sql = new mysqli("localhost", "root", "", "mindenapicpp");
+$sql->query("SET NAMES utf8");
+$result = $sql->query("SELECT * FROM questions ORDER BY RAND() LIMIT 1");
+$row = $result->fetch_assoc();
+
+$pack=array();
+
+$pack[] = array( 'id_question' =>$row['id_question'],
+                 'question'=> $row['question'],
+                 'answer_1'=> $row['answer_1'],
+                 'answer_2'=> $row['answer_2'],
+                 'answer_3'=> $row['answer_3'],
+                 'answer_4'=> $row['answer_4'],
+                 'correct_answer' => $row['correct_answer'],
+                 'difficulty'=> $row['difficulty']);
+
+$fp = fopen('results.json', 'w');
+fwrite($fp, json_encode($pack));
+fclose($fp);
---
+++ submitanswer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tamás
+ * Date: 11/26/2014
+ * Time: 8:40 PM
+ */
+
+
+// EXAMPLE submitanswer.php?index=14415166&id_question=3&correct=1
+
+if (isset($_GET['index']))
+    $index = $_GET['index'];
+if (isset($_GET['correct']))
+    $correct = $_GET['correct'];
+if (isset($_GET['id_question']))
+    $id_question = $_GET['id_question'];
+
+
+$sql = new mysqli("localhost", "root", "", "mindenapicpp");
+$sql->query("SET NAMES utf8");
+$result = $sql->query("SELECT id_member FROM users WHERE index_number='$index'");
+$row = $result->fetch_assoc();
+
+//var_dump($row['id_member']);
+$id_member = $row['id_member'];
+$mysql_date_now = date("Y-m-d");
+
+$sql->query("SET NAMES UTF8");
+$sql->query("INSERT INTO answers (id_question, correct, date, id_member)" . "VALUES ('$id_question', '$correct', '$mysql_date_now', '$id_member');");
