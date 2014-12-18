58cac154c8a8d2c9930f6c98ff984ed4c9e4f215
hlabadi <hlabadi@hotmail.com>
2014-12-18 23:32:53 +0100

    [EXPERIMENTAL] GUID added to JSON

--- randomquestion.php
+++ randomquestion.php
@@ -17,8 +17,27 @@ $pack = array( 'id_question' =>$row['id_question'],
                  'answer_3'=> $row['answer_3'],
                  'answer_4'=> $row['answer_4'],
                  'correct_answer' => $row['correct_answer'],
-                 'difficulty'=> $row['difficulty']);
+                 'difficulty'=> $row['difficulty']),
+		'guid' => guid();
 
 $fp = fopen('results.json', 'w');
 fwrite($fp, json_encode($pack));
 fclose($fp);
+
+function guid(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+                .substr($charid, 0, 8).$hyphen
+                .substr($charid, 8, 4).$hyphen
+                .substr($charid,12, 4).$hyphen
+                .substr($charid,16, 4).$hyphen
+                .substr($charid,20,12)
+                .chr(125);// "}"
+        return $uuid;
+    }
+}
