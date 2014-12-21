ce10893a398dbbd1a08727632ff1cc1051d0a65e
Birks <tommyb1@windowslive.com>
2014-12-21 18:53:49 +0100

    Small JS modification

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -1,41 +1,24 @@
 $(document).ready(function(){
     var questionTable = $('#questions_table');
-    var numOfRows = 0;
-    var firstTime = true;
     var limit = { bottom: 0, number: 15 };
     var loadingImg = $('#loading_div');
-    var nextBtn = document.getElementsByClassName('listButton')[0];
-    var prevBtn = document.getElementsByClassName('listButton')[1];
-
-    var initFunction = function(){
-        questionTable.html(loadingImg.html());
-        setTableWidth(true);
-
-        loadNumOfRows(0);
-        loadData(questionTable);
-    };
+    var nextBtn = document.getElementsByClassName('listButton')[1];
+    var prevBtn = document.getElementsByClassName('listButton')[0];
 
     nextBtn.onclick = function(){
-        limit.bottom += limit.number;
+        limit.bottom += 15;
         questionTable.html(loadingImg.html());
         setTableWidth(true);
-
-        loadNumOfRows(0);
-
         loadData(questionTable);
     };
 
     prevBtn.onclick = function(){
-        if(limit.bottom - limit.number >= 0){
-            limit.bottom -= limit.number;
+        if(limit.bottom - 15 >= 0) {
+            limit.bottom -= 15;
             questionTable.html(loadingImg.html());
             setTableWidth(true);
-
-            loadNumOfRows(-15);
             loadData(questionTable);
         }
-
-        if(limit.bottom - limit.number * 2 <= 0) prevBtn.style.display = "none";
     };
 
     var loadData = function(obj) {
@@ -56,43 +39,12 @@ $(document).ready(function(){
             questionTable.height(500);
             return 0;
         }
+
         questionTable.width('auto');
         questionTable.height('auto');
     };
 
-    var loadNumOfRows = function(offset){
-        offset = limit.bottom + limit.number + offset;
-
-        if(offset == 0) numOfRows = 0;
-        else{
-            $.ajax({
-                url: "php/GetNumOfRows.php?bottom=" + offset + "&number=" + limit.number + "&type=answers",
-                type: "GET",
-                cache: false,
-                success: function(html){
-                    numOfRows = html;
-                    setButtonVisibility();
-                }
-            });
-        }
-    };
-
-    var setButtonVisibility = function(){
-        if(numOfRows == 0) {
-            nextBtn.style.display = "none";
-        }
-        else {
-            nextBtn.style.display = "inline-block";
-            prevBtn.style.display = "inline-block";
-        }
-
-        if(firstTime) {
-            prevBtn.style.display = "none";
-            firstTime = false;
-        }
-    };
-
     questionTable.html(loadingImg.html());
     setTableWidth(true);
-    initFunction();
+    loadData(questionTable);
 });
